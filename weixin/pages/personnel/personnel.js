115789b9b89fa5b3cce95eedba5cd4c5d5251b29
小连 <53170933+1197829463@users.noreply.github.com>
2019-10-07 18:01:10 +0800

    tab选项卡页面的滑动与切换完成

--- weixin/pages/personnel/personnel.js
+++ weixin/pages/personnel/personnel.js
@@ -1,12 +1,20 @@
 Page({
   data: {
     focus: false,
-    inputValue: ''
+    inputValue: '',
+    scrollTop: 0
   },
   bindButtonTap: function () {
     this.setData({
       focus: true
     })
   },
- 
+  //滚轮监听
+  onPageScroll: function (e) {
+    var _this = this
+    console.log(e.scrollTop, this.data.scroll_top)
+    _this.setData({
+      'scroll_top': e.scrollTop
+    })
+  }
 })
--- weixin/pages/tab/tab.js
+++ weixin/pages/tab/tab.js
@@ -1,104 +1,90 @@
 Page({
 	data: {
-		list:[
-			{
-				name:'LOGO设计',
-			},
-			{
-				name:'标题二',
-			},
-			{
-				name:'标题3',
-			},
-			{
-				name:'标题第四个',
-			},
-			{
-				name:'标题五',
-			},
-			{
-				name:'标题六',
-			},
-			{
-				name:'标题7',
-			},
-			{
-				name:'标题一个',
-			},
-			{
-				name:'标题二',
-			},
-			{
-				name:'标题3',
-			},
-			{
-				name:'标题第四个',
-			},
-			{
-				name:'标题五',
-			},
-			{
-				name:'标题六',
-			},
-			{
-				name:'标题7',
-			},
-		],
-		line:{
-			width:40,
-			left:0,
-			oldActive:0,
-			swipeIndex:0,
-			scrLeft:0,
-			timeOut:''
-		}
+		showtab: 0, //顶部选项卡索引
+		tabnav: {
+			tabnum: 5,
+			tabitem: [
+				{
+					"id": 0,
+					"text": "LOGO设计"
+				},
+				{
+					"id": 1,
+					"text": "APP开发"
+				},
+				{
+					"id": 2,
+					"text": "网站建设"
+				},
+				{
+					"id": 3,
+					"text": "品牌营销"
+				},
+				{
+					"id": 4,
+					"text": "小程序开发"
+				},
+				{
+					"id": 5,
+					"text": "命名取名"
+				},
+				{
+					"id": 6,
+					"text": "PPT设计"
+				},
+				{
+					"id": 7,
+					"text": "代运营"
+				},
+				{
+					"id": 8,
+					"text": "公众平台开发"
+				},
+				{
+					"id": 9,
+					"text": "工业设计"
+				},
+				{
+					"id": 10,
+					"text": "动漫游戏"
+				},
+				{
+					"id": 11,
+					"text": "视频制作"
+				},
+				{
+					"id": 12,
+					"text": "更多"
+				},
+			]
+		},
+		productList: [],
+		// tab切换
+		currentTab: 0,
 	},
 	onLoad: function () {
-		var _this = this;
-		_this.lineInfo(0)
 	},
-	swipeChange:function(res){
-		var _this = this;
-		if(res.detail.source == 'touch'){
-			if(_this.data.line.timeOut){clearTimeout(_this.data.line.timeOut)}
-			_this.data.line.timeOut = setTimeout(function(){
-				_this.lineInfo(res.detail.current,true)
-			},10)
+	setTab: function (e) {
+		var that = this
+		that.setData({
+			showtab: e.currentTarget.dataset.tabindex
+		})
+		if (this.data.currentTab === e.currentTarget.dataset.tabindex) {
+			return false;
+		} else {
+			that.setData({
+				currentTab: e.currentTarget.dataset.tabindex
+			})
 		}
 	},
-	lineInfo:function(even,type){
-		var _this = this;
-		var index = even >= 0 ? even : even.currentTarget.id;
-		index = parseInt(index);
-		wx.getSystemInfo({
-			success: function(info) {
-				wx.createSelectorQuery().selectAll('.tabBtn').boundingClientRect(function(rect){
-					wx.createSelectorQuery().select('#tabButtonAll').scrollOffset(function(res){
-						var WinWidth = info.windowWidth;
-						var width = rect[index].width;
-						var left = rect[index].left;
-						var scrLeft = res.scrollLeft;
-						_this.setData({'line.width':width,'line.left':left + scrLeft})
-						if(_this.data.line.oldActive == index){
-						}else if(_this.data.line.oldActive < index){
-							if(left + width + (WinWidth/750*72)> WinWidth){
-								_this.setData({'line.scrLeft':rect[index-3].left + scrLeft})
-							}
-						}else{
-							console.log(left)
-							console.log(scrLeft)
-							if(scrLeft > left + scrLeft - (WinWidth/750*72)){
-								var i = index-1>0 ? rect[index-1].left + scrLeft : 0;
-								_this.setData({'line.scrLeft':i})
-							}
-						}
-						if(!type){
-							_this.setData({'line.swipeIndex':index})
-						}
-						_this.setData({'line.oldActive':index})
-					}).exec()
-				}).exec()
-			}
-		})
-	}
+	/**
+	* 滑动切换tab
+	*/
+	bindChange: function (e) {
+		var that = this;
+		that.setData({
+			currentTab: e.detail.current,
+			showtab: e.detail.current
+		});
+	},
 })
